f1a8f069096c114566c334564322c14f01dd549c
Adem Ahmet Karakaya <adem.a.karakaya@gmail.com>
2020-10-03 15:23:38 +1000

    Stray '0' in cpp; added mex file.

--- extractEyesOCVPoints.cpp
+++ extractEyesOCVPoints.cpp
@@ -72,7 +72,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, mxArray *prhs[])
     {
         Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
         //ellipse( *frame, center, Size( faces[i].width*0.5, faces[i].height*0.5), 0, 0, 360, Scalar( 255, 0, 255 ), 4, 8, 0 );
-0
+
         Mat faceROI = frame_gray( faces[i] );
         std::vector<Rect> eyes;
 
